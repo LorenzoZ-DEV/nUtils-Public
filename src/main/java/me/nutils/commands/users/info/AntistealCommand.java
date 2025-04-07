6f5dfe755d8d004aad31eb599cf9d5893e6d043f
Vanixy <116018011+LorenzoZ-DEV@users.noreply.github.com>
2025-04-07 21:34:42 +0200

    Refactor package structure and update version to 2.14-h1

--- src/main/java/me/nutils/commands/users/info/AntistealCommand.java
+++ src/main/java/me/nutils/commands/users/info/AntistealCommand.java
@@ -16,8 +16,8 @@ public class AntistealCommand implements SimpleCommand {
 
         p.sendMessage(Component.text(CS.translate("&#555555&m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")));
         p.sendMessage(Component.text(CS.translate("&#8a2be2&lPUBLIC &#555555» &#d633ffThis server Use &#ff66ccNUtils for Velocity&#aaaaaa.")));
-        p.sendMessage(Component.text(CS.translate("&#555555Versione: &#d633ff2.12 &#ff3333-Public")));
-        p.sendMessage(Component.text(CS.translate("&#555555Spigot Link: &#d633ffhttps://www.spigotmc.org/resources/ninfeaplus.100000/")));
+        p.sendMessage(Component.text(CS.translate("&#555555Versione: &#d633ff2.14-h1 &#ff3333-Public")));
+        p.sendMessage(Component.text(CS.translate("&#555555Spigot Link: &#d633ffhttps://www.spigotmc.org/resources/nutils.119755/")));
         p.sendMessage(Component.text(CS.translate("&#555555&m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")));
     }
 }
